1e4db2818e81f484b76d10b934b09e91afdcbf33
Adam Gall <adam@decentlabs.io>
2019-05-28 17:05:55 -0400

    Do the work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App App-header">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: null, desc: null }
+  }
+  
+  componentDidMount() {
+    const assetId = window.location.pathname.replace(/^\/|\/$/g, '');
+    let self = this;
+    return axios.get('https://xchain.io/api/asset/' + assetId).then(function(response) {
+      const data = response.data.description.split(';');
+      self.setState({ 
+        url: data[0].replace('imgur', 'https://i.imgur.com'),
+        desc: data[1] 
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App App-header">
+        { this.state.url && 
+          <div>
+            <div>
+              <img src={ this.state.url } alt="meme"></img>
+            </div>
+            <p>
+              { this.state.desc }
+            </p>
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
