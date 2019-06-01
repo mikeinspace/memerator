3b17f611480cd0e2f1deb01c80d0a49b5ee9cbb0
mikeinspace <30351874+mikeinspace@users.noreply.github.com>
2019-06-01 01:53:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
             <div><img src={ this.state.url } alt="meme"></img></div>
             <p>{ this.state.desc }</p>
   
-            <p class="xchainDetails">Asset Details: <a href="javascript:window.open('https://xchain.io/asset'+window.location.pathname, 'yourWindowName', 'width=200,height=150');">https://xchain.io/asset{ window.location.pathname }</a></p>
+            <p class="xchainDetails">Asset Details: <a href="javascript:location.href('https://xchain.io/asset'+window.location.pathname);">https://xchain.io/asset{ window.location.pathname }</a></p>
       
 
 
