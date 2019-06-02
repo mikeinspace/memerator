4e54c88b8ca1eed6f740d0975cf4fe027351b2b9
Adam Gall <adam@decentlabs.io>
2019-06-02 18:46:26 -0400

    Clean up whitespace

--- src/App.js
+++ src/App.js
@@ -2,75 +2,35 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { url: null, desc: null }
   }
-  
 
-  
   componentDidMount() {
     const assetId = window.location.pathname;
     let self = this;
-    return axios.get('https://xchain.io/api/asset' + assetId).then(function(response) {
-      
+    return axios.get('https://xchain.io/api/asset' + assetId).then(function (response) {
       const data = response.data.description.split(';');
-      self.setState({ 
+      self.setState({
         url: data[0].replace('imgur', 'https://i.imgur.com'),
         desc: data[1]
-        
-
       });
     });
   }
-  
-
-  
-  
-  
-  
-  
-  
 
   render() {
     return (
-        
-        
-
-      
-      
-      
-      
-       
       <div className="App App-header">
-      
-      
-      <h1>Viewport.me</h1>
-      
-      
-<div id="SearchField">Freeport Asset ID: <input type="text" id="text"/> <a href="javascript:window.location.href=document.getElementById('text').value;">GO</a></div>
-      
-      
-      
-      
-      
-        { this.state.url && 
+        <h1>Viewport.me</h1>
+        <div id="SearchField">Freeport Asset ID: <input type="text" id="text" /> <a href="javascript:window.location.href=document.getElementById('text').value;">GO</a></div>
+        {this.state.url &&
           <div>
-            <div><img src={ this.state.url } alt="meme"></img></div>
-            <p>{ this.state.desc }</p>
-  
-            <p class="xchainDetails">Asset Details: <a href="javascript:window.location.href='https://xchain.io/asset'+window.location.pathname;">xchain.io/asset{ window.location.pathname }</a></p>
-      
-
-
-
-      
-      
-               <p class="smallNotice">Create your own Cryptogoods at <a href="http://freeport.io" target="_blank">Freeport.io</a>!</p>
-      
-
+            <div><img src={this.state.url} alt="meme"></img></div>
+            <p>{this.state.desc}</p>
+            <p class="xchainDetails">Asset Details: <a href="javascript:window.location.href='https://xchain.io/asset'+window.location.pathname;">xchain.io/asset{window.location.pathname}</a></p>
+            <p class="smallNotice">Create your own Cryptogoods at <a href="http://freeport.io" target="_blank">Freeport.io</a>!</p>
           </div>
         }
       </div>
