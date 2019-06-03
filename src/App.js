e3f3f6b675167c1d2eec7d9ccc4d4d2c875f034b
Adam Gall <adam@decentlabs.io>
2019-06-02 22:48:14 -0400

    Remove all custom styling, replace with bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const imgurUrl = 'https://i.imgur.com';
 const xChainAssetBase = 'https://xchain.io/asset/';
