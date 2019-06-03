46305006f89fae7033c97ee4d84f000113a0e6d9
Adam Gall <adam@decentlabs.io>
2019-06-02 20:00:08 -0400

    Create state property for xChainUrl, and use it in the Asset Details anchor tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       imgUrl: null,
+      xChainUrl: null,
       desc: null,
     }
   }
@@ -18,6 +19,7 @@ class App extends React.Component {
       const data = response.data.description.split(';');
       self.setState({
         imgUrl: data[0].replace('imgur', 'https://i.imgur.com'),
+        xChainUrl: 'https://xchain.io/asset' + assetId,
         desc: data[1]
       });
     });
@@ -32,7 +34,7 @@ class App extends React.Component {
           <div>
             <img src={this.state.imgUrl} alt="meme" />
             <p>{this.state.desc}</p>
-            <p class="xchainDetails">Asset Details: <a href="javascript:window.location.href='https://xchain.io/asset'+window.location.pathname;">xchain.io/asset{window.location.pathname}</a></p>
+            <p class="xchainDetails">Asset Details: <a href={this.state.xChainUrl}>{this.state.xChainUrl}</a></p>
             <p class="smallNotice">Create your own Cryptogoods at <a href="http://freeport.io" target="_blank">Freeport.io</a>!</p>
           </div>
         }
