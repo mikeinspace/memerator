03ea0c3ea65517352989faec8045621fa76f8258
mikeinspace <30351874+mikeinspace@users.noreply.github.com>
2019-07-09 13:58:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ function footer() {
       <div className="col small">
         <span>Create your own Cryptogoods with </span>
         <a className="text-light" href="https://freeport.io" target="_blank" rel="noopener noreferrer">Freeport.io</a>
-        <span>!<br/>Enjoy this site? Cryptogoods happily accepted here: 1GotRejB6XsGgMsM79TvcypeanDJRJbMtg<br/><br/>Contact me on Twitter: <a className="text-light" href="https://www.twitter.com/mikeinspace" target="_blank" rel="noopener noreferrer">@mikeinspace</a><br/><br/>Special thanks to <a className="text-light" href="https://www.twitter.com/trainface" target="_blank" rel="noopener noreferrer">@trainface</a> for the dev work.</span>
+        <span>!<br/>Enjoy this site? Cryptogoods happily accepted here: 1GotRejB6XsGgMsM79TvcypeanDJRJbMtg<br/><br/>Special thanks to <a className="text-light" href="https://www.twitter.com/trainface" target="_blank" rel="noopener noreferrer">@trainface</a> for the dev assistance.<br/>Contact me on Twitter: <a className="text-light" href="https://www.twitter.com/mikeinspace" target="_blank" rel="noopener noreferrer">@mikeinspace</a></span>
       </div>
     </div>
   );
